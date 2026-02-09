0fbccbccbf241783f35d4c86401649555e5604ad
Luc Lagrange <luc.lagrange@converteo.com>
2026-02-09 11:47:14 +0100

    feat: postgres creation and script update to insert into postgres

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# data_engineer_onboarding
+# What is this?
 Data Engineering onboarding exercise
+
+# Setup
+
+### 1. Set the variables
+Write all your variables in a .env file and use the following command to export them all at once: 
+
+```bash
+export $(grep -v '^#' .env | xargs)
+```
+
+### 2. Build and run the Postgres container
+```bash
+cd server/
+docker compose build
+docker compose up -d
+```
+
+### 3. Build and run the fetch & export containers 
+
+Depending on which method you want to try:
+
+1. "Classic" python method with requests and psycopg
+
+```bash
+cd client-1
+docker compose build
+docker compose up
+```
+
+2. dltHub method
+
+```bash
+cd client-2
+docker compose build
+docker compose up
+```
--- client-1/src/job/main.py
+++ client-1/src/job/main.py
@@ -2,13 +2,21 @@ import requests
 import logging
 import os
 import datetime
+import psycopg
 from typing import Any, Dict, Optional
 from timeit import default_timer as timer
 
-# Configuration with type hints
-OPEN_WEATHER_MAP_API_KEY: Optional[str] = os.getenv("OPEN_WEATHER_MAP_API_KEY")
-LATITUDE: Optional[str] = os.getenv("LATITUDE")
-LONGITUDE: Optional[str] = os.getenv("LONGITUDE")
+# Script Configuration
+OPEN_WEATHER_MAP_API_KEY = os.getenv("OPEN_WEATHER_MAP_API_KEY").strip()
+LATITUDE = os.getenv("LATITUDE").strip()
+LONGITUDE = os.getenv("LONGITUDE").strip()
+
+# DB Variables
+DB_HOST = os.getenv("DB_HOST").strip()
+DB_USER = os.getenv("DB_USER").strip()
+DB_PASS = os.getenv("DB_PASS").strip()
+DB_NAME = os.getenv("DB_NAME").strip()
+DB_PORT = os.getenv("DB_PORT").strip()
 
 # Configure the logging module
 logging.basicConfig(
@@ -150,6 +158,35 @@ def extract_weather_information_from_json(data: Dict[str, Any]) -> Dict[str, Any
     return weather_info
 
 
+def save_to_db(info: Dict[str, Any]) -> bool:
+    """Insert the weather data into PostgreSQL 18."""
+    conn_str = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+    query = """
+    INSERT INTO weather_metrics (weather, description, temperature, humidity, observed_at)
+    VALUES (%s, %s, %s, %s, %s);
+    """
+
+    try:
+        with psycopg.connect(conn_str) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    query,
+                    (
+                        info["weather"],
+                        info["description"],
+                        info["temperature"],
+                        info["humidity"],
+                        info["date"],
+                    ),
+                )
+        logging.info("Successfully ingested data into Postgres.")
+        return True
+    except psycopg.Error as e:
+        logging.error("Database insertion failed: %s", e)
+        return False
+
+
 def main() -> None:
     """
     Main execution logic to fetch and display weather information.
@@ -163,6 +200,7 @@ def main() -> None:
     # Check if data exists before extraction to avoid errors
     if data:
         weather_information = extract_weather_information_from_json(data)
+        save_to_db(weather_information)
 
     end = timer()
     duration = round(end - start, 1)
